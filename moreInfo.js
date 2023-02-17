dac08ad844f268036a29d7e66d1e37a362e0b5d9
jean222 <jeanamante22@gmail.com>
2023-02-16 17:59:30 -0800

    .

--- moreInfo.js
+++ moreInfo.js
@@ -4,5 +4,9 @@ let userInfo = JSON.parse(localStorage.getItem('body'));
 
 
 for(let i = 0; i < userInfo.length; i++){
-    
+    document.getElementById('info').innerHTML += `<div>
+    <h2>${userInfo[i].part}</h2>
+    <h4>${userInfo[i].summary}</h4>
+    <p>${userInfo[i].description}</p>
+    </div>`
 }
