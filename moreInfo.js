4a9971141f4f687d42af8af5a25c9de1d027aba1
Jean <105907450+jean222@users.noreply.github.com>
2023-02-16 19:36:56 -0800

    tried fixing moreInfo page but gonna move to profile page

--- moreInfo.js
+++ moreInfo.js
@@ -1,12 +1,18 @@
-
-
 let userInfo = JSON.parse(localStorage.getItem('body'));
 
-
 for(let i = 0; i < userInfo.length; i++){
-    document.getElementById('info').innerHTML += `<div>
-    <h2>${userInfo[i].part}</h2>
-    <h4>${userInfo[i].summary}</h4>
-    <p>${userInfo[i].description}</p>
+    document.getElementById('info').innerHTML += `
+    <div>
+        <h2>
+            <ul>
+            ${userInfo[i].part}
+            </ul>
+        </h2>
+
+        <h4>
+            ${userInfo[i].summary}
+        </h4>
+
+        <p>${userInfo[i].description}</p>
     </div>`
 }
